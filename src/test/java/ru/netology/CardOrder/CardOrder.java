4a7128099b8e505e2166a1cbe7cdaa45c14ed8cd
Maria Vostrikova <vostrikova.maria2017@yandex.ru>
2021-11-26 01:16:09 +0300

    field date is cleared

--- src/test/java/ru/netology/CardOrder/CardOrder.java
+++ src/test/java/ru/netology/CardOrder/CardOrder.java
@@ -4,10 +4,13 @@ import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+
 import java.time.Duration;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.*;
 
@@ -27,12 +30,13 @@ return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.
         SelenideElement form = $("[action]");
         form.$("[data-test-id = city] input").setValue("Москва");
         String futureDate = date(3);
+        form.$("[data-test-id='date'] input").doubleClick().sendKeys(Keys.BACK_SPACE);
         form.$("[data-test-id=date] input").setValue(futureDate);
         form.$("[data-test-id=name] input").setValue("Иван Петров");
         form.$("[data-test-id=phone] input").setValue("+79125345678");
         form.$("[data-test-id=agreement] .checkbox__box").click();
         $$("button").find(Condition.exactText("Забронировать")).click();
-        $(withText("Успешно!")).shouldBe(visible, Duration.ofSeconds(15));
+        $(byText("Успешно!")).shouldBe(visible, Duration.ofSeconds(15));
         $(".notification__content").shouldBe(visible, Duration.ofSeconds(15)).shouldHave(Condition.exactText("Встреча успешно забронирована на " + futureDate));
     }
 }
